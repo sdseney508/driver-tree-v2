c440ebb308e420f9ceb86d53d2f247ea6d2de097
Steven Seney <sdseney508@gmail.com>
2023-10-13 15:57:22 -0400

    fixed api address

--- client/src/utils/apiURL.js
+++ client/src/utils/apiURL.js
@@ -2,6 +2,6 @@
 // const apiURL = "http://localhost:8080/api";
 
 //for deployed build
-const apiURL = "https://driver-tree-830b009d7ab7.herokuapp.com/api/";
+const apiURL = "https://driver-tree-830b009d7ab7.herokuapp.com/api";
 // 
 export default apiURL;
