d450179d54e19d0ea73b1d1a15b7eff2c0748dbb
Steven Seney <sdseney508@gmail.com>
2024-01-01 13:27:21 -0500

    changed .env for AWS hosting

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,6 +8,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
+    process.env.DB_HOST,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
